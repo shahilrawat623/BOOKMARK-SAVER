4669955266f06d9416ae9c6c5590383dc131047b
shahil rawat <shahilr623@gmail.com>
2026-07-13 11:35:35 +0530

    Select DOM element for bookmark Functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+const addBookmarkBtn = document.getElementById("add-bookmark");
+const bookmarkList = document.getElementById("bookmark-list");
+const bookmarkNameInput = document.getElementById("bookmark-name");
+const bookmarkUrlInput = document.getElementById("bookmrk-url");
+
